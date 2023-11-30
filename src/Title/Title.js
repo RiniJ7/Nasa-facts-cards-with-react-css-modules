01eeba7c431923052ed5b9a5e2eca65cbb15f715
rinij <rinimani7@gmail.com>
2023-11-30 11:32:28 -0700

    changed Title from functional to class component

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import styles from './Title.module.css';
+// import React from 'react';
+// import styles from './Title.module.css';
 
-const Title = () => {
-  return (
-    <div>
-      <h1 className={styles.title}>Title</h1>
-    </div>
-  );
+// const Title = () => {
+//   return (
+//     <div>
+//       <h1 className={styles.title}>Title</h1>
+//     </div>
+//   );
+// }
+// export default Title;
+
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Title</h1>
+      </div>
+    );
+  }
 }
-export default Title;
