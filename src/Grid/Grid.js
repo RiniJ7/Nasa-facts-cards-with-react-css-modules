d6c3296142498e3e3a3a592e39045f6f70a7d1b0
rinij <rinimani7@gmail.com>
2023-11-30 11:33:27 -0700

    set up grid component to access the json data

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import GridItem from "./GridItem";
+
+import styles from './Grid.module.css';
 
 export default class App extends React.Component {
+  renderFacts() {
+    return this.props.facts.map((item) => (
+      <GridItem key={item.id} fact={item} />
+    ));
+  }
   render() {
     return (
-      <div>
-        <h1>Stateful component</h1>
-      </div>
+   <section className={styles.grid}>
+    {this.renderFacts()}
+   </section>
     );
   }
 }
