a6c2763dc5517da181852fbe9f58c3a507e76575
rinij <rinimani7@gmail.com>
2023-11-29 17:19:07 -0700

    components imported and used in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 
 import styles from './App.Module.css';
+import Title from './Title/Title';
+import Background from './Background/Background';
+import Grid from './Grid/Grid';
+import Rocket from './Rocket/Rocket';
+import Smoke from './Rocket/Smoke';
+
 
 function App() {
   return (
     <div className={styles.App}>
+      <Title/>
+      <Background/>
+      <Grid/>
+      <Rocket/>
+      <Smoke/>
     </div>
   );
 }
