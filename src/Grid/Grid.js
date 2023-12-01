b5d3cb24ec4c5afb73fe2f4e3a939a2484ec1471
rinij <rinimani7@gmail.com>
2023-12-01 13:01:33 -0700

    loaded svgs with state

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -4,9 +4,12 @@ import GridItem from "./GridItem";
 import styles from './Grid.module.css';
 
 export default class App extends React.Component {
+  updateClicked(id){
+    this.props.updateClicked(id);
+  }
   renderFacts() {
     return this.props.facts.map((item) => (
-      <GridItem key={item.id} fact={item} />
+      <GridItem key={item.id} fact={item} updateClicked={this.updateClicked.bind(this)} />
     ));
   }
   render() {
