253aa0a6161af51270040036b52e8b4ddc342593
rinij <rinimani7@gmail.com>
2024-04-03 01:05:24 -0600

    rocket image configured

--- src/Rocket/Rocket.js
+++ src/Rocket/Rocket.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from './Rocket.module.css';
-
+import shuttleImage from './Assets/shuttle.png';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <img src="./shuttle.png" alt="shuttle" className={styles.img}/>
+        <img src={shuttleImage} alt="shuttle" className={styles.img}/>
       </div>
     );
   }
