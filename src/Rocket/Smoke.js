47bb35cba5d9d9b1c7d70658372a0db8cc8d64e3
rinij <rinimani7@gmail.com>
2024-04-03 01:05:06 -0600

    smoke video and sound configured

--- src/Rocket/Smoke.js
+++ src/Rocket/Smoke.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import styles from './Smoke.module.css';
+import styles from './Smoke.Module.css';
+import smokeVideo from './Assets/smoke.mov';
+import smokeSound from './Assets/NASA.mp3';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className={styles.smoke}> 
-        <video className={styles.video} autoPlay loop src="/smoke.mov" />
+        <video className={styles.video} autoPlay loop> 
+        <source src={smokeVideo} type="video/mp4" />
+          Your browser does not support the video tag.
+          </video>
+          <audio autoPlay>
+          <source src={smokeSound} type="audio/mp3" />
+          </audio>
       </div>
     );
   }
